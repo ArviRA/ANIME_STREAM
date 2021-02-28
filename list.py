15eaa4323ae74dda25289e2797ae0f3f7073d8fc
Arvi <33565856+ArviRA@users.noreply.github.com>
2021-02-28 14:53:15 +0530

    updated

---
+++ list.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+send = {}
+raw_link = requests.get("https://gogo-play.net/")
+
+#print(raw_link)
+
+soup = BeautifulSoup(raw_link.content,"html.parser")
+
+dummy =soup.find_all('li',{'class':'video-block'})
+for i in range(0,len(dummy)):
+    picture = dummy[i].find('div',{'class':'picture'})
+    picture = picture.find('img')
+    description = picture.get('alt')
+    pic_link = picture.get('src')
+    name = dummy[i].find('div',{'class':'name'}).text.strip()
+    date = dummy[i].find('span',{'class':'date'}).text
+    dummy_dic = {}
+    dummy_dic['picture'] = pic_link
+    dummy_dic['descripton'] = description
+    dummy_dic['date'] = date
+    send[str(name)] = dummy_dic
+
+print(send)
+
--- trail.py
+++ trail.py
@@ -17,6 +17,35 @@ def get_link():
     except:
         return {'status':404}
 
+@app.route('/get_list',methods = ['GET','POST'])
+def get_list():
+    try:
+        print("inside")
+        link = dict(request.args)
+        raw = link['raw_link']
+        send = {}
+        raw_link = requests.get(raw)
+        soup = BeautifulSoup(raw_link.content,"html.parser")
+        dummy =soup.find_all('li',{'class':'video-block'})
+        for i in range(0,len(dummy)):
+            picture = dummy[i].find('div',{'class':'picture'})
+            picture = picture.find('img')
+            description = picture.get('alt')
+            pic_link = picture.get('src')
+            name = dummy[i].find('div',{'class':'name'}).text.strip()
+            date = dummy[i].find('span',{'class':'date'}).text
+            dummy_dic = {}
+            dummy_dic['picture'] = pic_link
+            dummy_dic['descripton'] = description
+            dummy_dic['date'] = date
+            send[str(name)] = dummy_dic
+        return {'status':200,'result':send}
+    except:
+        return {'status':404}
+        
+
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
