620a253c5eeccb6575c7d1c796508aabe79317b3
Arvi <33565856+ArviRA@users.noreply.github.com>
2021-03-01 22:09:37 +0530

    done

--- trail.py
+++ trail.py
@@ -9,14 +9,37 @@ CORS(app)
 @app.route('/get_frame',methods = ['GET','POST'])
 def get_link():
     try:
+        list_dict = {}
+        send = {}
         print(request.args)
         link = dict(request.args)
         raw_link = link['raw_link']
         link = requests.get(raw_link)
         soup = BeautifulSoup(link.content,"html.parser")
+        dummy =soup.find_all('li',{'class':'video-block'})
+        for i in range(0,len(dummy)):
+            dummy_dict = {}
+            picture = dummy[i].find('div',{'class':"picture"})
+            picture  = picture.find('img')
+            pic_link = picture.get('src')
+            href = dummy[i].find('a').get('href')
+            description = picture.get('alt')
+            name = dummy[i].find('div',{'class':'name'}).text.strip()
+            date = dummy[i].find('span',{'class':'date'}).text
+            dummy_dict['href'] = href
+            dummy_dict['pic_link']  =pic_link
+            dummy_dict['description']  =description
+            dummy_dict['date'] = date
+            list_dict[str(name)] = dummy_dict
+        #print(list_dict)
         src = soup.find_all('iframe')
-        add = "http:"+str(src[0]['src']).split('&')[0]
-        return {'status':200,'link':add}
+        send['frame_link']= "http:"+str(src[0]['src']).split('&')[0]
+        details = soup.find("div",{'class':'video-details'})
+        epi_name = details.find('span',{'class':'date'}).text.strip()
+        epi_description = details.find('div',{'class':'content-more-js'}).text.strip()
+        send['episode_name'] = epi_name
+        send['episode_description'] = epi_description
+        return {'status':200,'in_this_frame':send,'below_episodes':list_dict}
     except:
         return {'status':404}
 
